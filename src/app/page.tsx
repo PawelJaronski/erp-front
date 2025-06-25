8c8cc4d82347f7fa8a26a9c6cc569213ea760813
Paweł Jaroński <pawel@jaronski.pl>
2025-06-25 16:33:57 +0200

    just some tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SimpleExpenseForm from "@/components/SimpleExpenseForm";
 export default function Home() {
   return (
     <main>
-      <h1 className="bg-red-500 text-white p-4">ERP App</h1>
+      <h1>ERP App</h1>
       <SimpleExpenseForm />
     </main>
     
