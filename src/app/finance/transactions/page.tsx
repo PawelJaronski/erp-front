f3bcd7c1aa424ba66282769c01e83681219a40f4
Paweł Jaroński <pawel@jaronski.pl>
2025-07-17 20:28:36 +0200

    Transaction list

--- src/app/finance/transactions/page.tsx
+++ src/app/finance/transactions/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 
 import { useTransactionsFilters } from '@/features/transactions/hooks/useTransactionFilters'
 import { useTransactionsQuery } from '@/features/transactions/hooks/useTransactionsQuery'
